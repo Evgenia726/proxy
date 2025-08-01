2e128ee166ad58a5c46bc1a5b1dd9343d2f39ba5
Evgenia726 <mur_e00@mail.ru>
2025-08-01 17:57:08 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -9,12 +9,16 @@ app.get("/api", async (req, res) => {
   try {
     const response = await fetch(url, {
       headers: {
-        "User-Agent": "Mozilla/5.0",
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36",
+        "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
+        "Accept-Language": "ru-RU,ru;q=0.9,en-US;q=0.8,en;q=0.7",
+        "Referer": "https://www.google.com/",
+        "Connection": "keep-alive",
       },
     });
 
-    const text = await response.text();
-    res.send(text);
+    const html = await response.text();
+    res.send(html);
   } catch (err) {
     res.status(500).json({
       error: "Something went wrong",
@@ -31,3 +35,4 @@ const PORT = process.env.PORT || 10000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
+
